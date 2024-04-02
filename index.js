0d6ad7a54b8b62eeab354a35a31c720fa2e99de2
achorn <joshuaachorn@gmail.com>
2024-04-01 19:55:37 -0700

    added create and join game

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const http = require("http");
+const { client } = require("websocket");
 const app = require("express")();
-app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 app.listen(9091, () => console.log("listening on http port 9091"));
+
 const webSocketServer = require("websocket").server;
 const httpServer = http.createServer();
 httpServer.listen(9090, () => console.log("listening... on 9090"));
 
 const clients = {};
+const games = {};
 // // clients[clientId]
 
 const wsServer = new webSocketServer({
@@ -22,7 +25,45 @@ wsServer.on("request", (request) => {
   connection.on("message", (message) => {
     const result = JSON.parse(message.utf8Data);
     //i have received a message form client
-    console.log(result);
+
+    //user wants to create new game
+    if (result.method === "create") {
+      const clientId = result.clientId;
+
+      const gameId = guid();
+      games[gameId] = {
+        id: gameId,
+        balls: 20,
+        clients: [],
+      };
+      const payLoad = {
+        method: "create",
+        game: games[gameId],
+      };
+      const con = clients[clientId].connection;
+      con.send(JSON.stringify(payLoad));
+    }
+
+    if (result.method === "join") {
+      const clientId = result.clientId;
+      const gameId = result.gameId;
+      const game = games[gameId];
+      if (game.clients.length >= 3) {
+        //sorry max players reach
+        return;
+      }
+      const color = { 0: "Red", 1: "Green", 2: "Blue" }[game.clients.length];
+      game.clients.push({ clientId: clientId, color: color });
+
+      const payLoad = {
+        method: "join",
+        game: game,
+      };
+      // loop through all clients and update everyone one
+      game.clients.forEach((client) => {
+        clients[client.clientId].connection.send(JSON.stringify(payLoad));
+      });
+    }
   });
   //generate new clientId
   const clientId = guid();
